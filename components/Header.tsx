9599508463429b0b3323e8ae9e6b9d710fa126bb
Haris <mh47098@gmail.com>
2025-10-30 15:48:32 +0000

    initial commit

---
+++ components/Header.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Container from "./Container";
+import Logo from "./Logo";
+
+const Header = () => {
+  return (
+    <header className="bg-white py-5">
+      <Container className="">
+        {/*Logo */}
+        <Logo />
+        {/*Menu Items  */}
+        {/*Nav Admin  */}
+      </Container>
+    </header>
+  );
+};
+
+export default Header;
+Header;
